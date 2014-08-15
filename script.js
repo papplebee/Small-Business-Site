b9a19fc627241e1a13e568be79d670ec125ff52c
Peter Applebee <peterapplebee@gmail.com>
2014-08-15 20:07:22 +0100

    Removed Mouse Leave
    Removed mouse leave from jQuery, because it makes the menu never display
    on touch device.

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
     jQuery(document).ready(function() {    
         
         $('div#menu-button').on('click', function() {
-                var menu = $(this).find('ul')
                 //toggle the menu on click
-                menu.slideToggle(100);
-                //slide up the menu on mouse leave
-                $(this).on('mouseleave', function() {
-                	menu.slideUp(100);
-                });
+                $(this).find('ul').slideToggle(100);
+                
             });
 
 
